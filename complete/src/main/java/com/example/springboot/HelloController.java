2549759ea2baf8672a1a1ebe82a8e4d0727eddc6
vaughn-redcloud3 <62781294+vaughn-redcloud3@users.noreply.github.com>
2020-05-05 17:33:00 -0700

    Change 2

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! Change 1";
+		return "Greetings from Spring Boot! Change 2";
 	}
 
 }
